0d1bca9bbcf5a6c92b089f433a67d702ce4bbbc9
rubendejager <r0851300@student.thomasmore.be>
2022-03-21 22:51:34 +0100

    without

--- components/Countries.js
+++ components/Countries.js
@@ -1,37 +1,56 @@
-import {useQuery} from "react-query";
 import {FlatList, LogBox, Text, View, Dimensions, StyleSheet} from "react-native";
 import Country from "./Country";
+import {useEffect, useState} from "react";
 
 LogBox.ignoreLogs(['Setting a timer']);
 
-const fetchCountries = async () => {
-    const result = await fetch("https://restcountries.com/v3.1/all");
-    return await result.json();
-}
+
 
 export default function Countries() {
-    const {data, status} = useQuery('countries', fetchCountries)
+    const [countries, setCountries] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+    const [isError, setError] = useState(false);
 
-    return <View>
-        {status === 'loading' && (
+    const fetchCountries = async () => {
+        setError(false);
+        setLoading(true);
+        try {
+            const response = await fetch("https://restcountries.com/v3.1/all");
+            const responseJSON = await response.json();
+            setCountries(responseJSON);
+        } catch (error) {
+            setError(true);
+            console.log(error)
+        }
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchCountries();
+    }, [])
+
+    return <View style={styles.flatlistContainer}>
+        {isLoading &&
             <Text>Data is loading...</Text>
-        )}
+        }
 
-        {status === 'error' && (
+        {isError &&
             <Text>Error while fetching the data</Text>
-        )}
+        }
 
-        {status === 'success' && (
-            <View style={styles.flatlistContainer}>
-                <FlatList data={data} renderItem={Country} keyExtractor={country => country.name.common}/>
+        {!isLoading && !isError &&
+            <View>
+                <FlatList data={countries} renderItem={Country} keyExtractor={country => country.name.common}/>
             </View>
-        )}
+        }
+
+
     </View>
 }
 
 const styles = StyleSheet.create({
     flatlistContainer: {
         width: Dimensions.get("window").width,
-        marginTop: 10
+        marginTop: 24
     }
 })
