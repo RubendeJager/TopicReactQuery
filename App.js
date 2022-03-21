6abef1f9e8d55163b4aeb298ae6b954b904ad7e0
rubendejager <r0851300@student.thomasmore.be>
2022-03-21 23:04:11 +0100

    clean start

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StyleSheet, View } from 'react-native';
-import {QueryClient, QueryClientProvider} from "react-query";
 import Countries from "./components/Countries";
-const queryClient = new QueryClient()
 export default function App() {
   return (
-      <QueryClientProvider client={queryClient}>
         <View style={styles.container}>
           <Countries/>
         </View>
-      </QueryClientProvider>
   );
 }
 
--- components/Countries.js
+++ components/Countries.js
@@ -1,30 +1,16 @@
-import {useQuery} from "react-query";
 import {FlatList, LogBox, Text, View, Dimensions, StyleSheet} from "react-native";
 import Country from "./Country";
 
 LogBox.ignoreLogs(['Setting a timer']);
 
-const fetchCountries = async () => {
-    const result = await fetch("https://restcountries.com/v3.1/all");
-    return await result.json();
-}
 
 export default function Countries() {
-    const {data, status} = useQuery('countries', fetchCountries)
 
     return <View>
-        {status === 'loading' && (
-            <Text>Data is loading...</Text>
-        )}
 
-        {status === 'error' && (
-            <Text>Error while fetching the data</Text>
-        )}
-
-        {status === 'success' && (
-            <View style={styles.flatlistContainer}>
-                <FlatList data={data} renderItem={Country} keyExtractor={country => country.name.common}/>
-            </View>
+        {/*<View style={styles.flatlistContainer}>
+            <FlatList data={data} renderItem={Country} keyExtractor={country => country.name.common}/>
+        </View>*/}
         )}
     </View>
 }
